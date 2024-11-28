eda8c6236428fcb5d7a84a91773b6aeea0bedbb9
unknown <willreubena@gmail.com>
2024-11-28 11:43:12 +0000

    added new files

---
+++ scripts/homepageScripts.js
@@ -0,0 +1,19 @@
+
+
+// Login
+const loginForm = document.querySelector('.login-form')
+
+loginForm.addEventListener('submit', (event) => {
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(jsonData)
+    })
+    .then(response => response.json)
+    .then(data => {
+        console.log("Response from server:", data)
+    })
+    .catch(error => console.error("Error:", error))
+})
--- scripts/taskboardScripts.js
+++ scripts/taskboardScripts.js
@@ -334,11 +334,11 @@ fetch(taskboardUrl)
                 },
                 body: JSON.stringify(jsonData)
             })
-                .then(response => response.json())
-                .then(data => {
-                    console.log("Response from server:", data)
-                })
-                .catch(error => console.error("Error:", error))
+            .then(response => response.json())
+            .then(data => {
+                console.log("Response from server:", data)
+            })
+            .catch(error => console.error("Error:", error))
         })
 
         // Dynamic title input code
